a562e6bb7a54cb3c84a5fa77a5ba81ca85a73d36
Lilcheo <34287554+lilcheo@users.noreply.github.com>
2020-03-28 18:08:12 -0700

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,1 +1,18 @@
+module.exports = {
+  name: "ping",
+  description: "Returns latency and API ping",
+  run: async (client, message, args) => {
+    clet totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+    
+    let clap = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+    
+ const msg = await message.channel.send(`Checking...`);
 
+    msg.edit(`${clap}`);
+  }
+};
