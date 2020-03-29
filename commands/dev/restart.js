6da555b9ac3c20894d5aaf86710246ff5f68e048
Lilcheo <34287554+lilcheo@users.noreply.github.com>
2020-03-28 19:21:56 -0700

    Update restart.js

--- commands/dev/restart.js
+++ commands/dev/restart.js
@@ -1,1 +1,22 @@
+const { main } = require("./owner.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+module.exports = {
+  name: "restart",
+  description: "idk",
+  run: async (client, message, args) => {
+
+ let owner = "275350295293198337";
+
+
+ if(message.author.id !== owner) return;
+    try {
+    process.exit();
+    }
+    
+ catch (err) {
+      console.log(err)
+ }
+  }
+}
