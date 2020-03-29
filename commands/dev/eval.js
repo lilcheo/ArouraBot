4a66e88ee603820b2aee622df9f540fc33bef186
Lilcheo <34287554+lilcheo@users.noreply.github.com>
2020-03-28 18:23:54 -0700

    Create eval.js

---
+++ commands/dev/eval.js
@@ -0,0 +1,34 @@
+const { main } = require("/owner.json");
+module.exports = {
+  name: "ping",
+  description: "Returns latency and API ping",
+  run: async (client, message, args) => {
+  
+const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+ 
+  if (message.content.startsWith(main.prefix + "eval")) {
+    if(message.author.id !== main.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
+
+  }
+}
