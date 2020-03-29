fb5ca3f8252048a7b3735ba372ec369e3351e219
Lilcheo <34287554+lilcheo@users.noreply.github.com>
2020-03-28 18:09:38 -0700

    PatchedGlitch issue #1

--- handlers/command.js
+++ handlers/command.js
@@ -24,7 +24,7 @@ module.exports = client => {
         );
         continue;
       }
-
+//glitches fixed here too
       if (pull.aliases && Array.isArray(pull.aliases))
         pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
     }
