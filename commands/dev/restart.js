16eada1aa451b629d4d68a55e732193e94121888
Lilcheo <34287554+lilcheo@users.noreply.github.com>
2020-03-29 17:53:15 -0700

    Update restart.js

--- commands/dev/restart.js
+++ commands/dev/restart.js
@@ -11,7 +11,7 @@ let owner = "275350295293198337";
  if(message.author.id !== owner){
 message.channel.send("error: ${console.error}")
 }else {
-message.channel.send("Restarting").then(() => process.exit())
+message.channel.send("Restarting...").then(() => process.exit())
 }
   }
 }
